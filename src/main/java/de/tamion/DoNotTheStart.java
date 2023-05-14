8b82f735c68c6df6099560fdcfc1761658f0e5b9
Tamion <simon.dorner.obf@gmail.com>
2023-05-14 09:15:31 +0200

    Forgor to push xxd

--- src/main/java/de/tamion/DoNotTheStart.java
+++ src/main/java/de/tamion/DoNotTheStart.java
@@ -1,10 +1,26 @@
 package de.tamion;
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 public final class DoNotTheStart extends JavaPlugin {
     @Override
     public void onEnable() {
-        Bukkit.shutdown();
-
+        for(Plugin pl : Bukkit.getPluginManager().getPlugins()) {
+            if(Bukkit.getPluginManager().getPlugins().length==1) {
+                Bukkit.getConsoleSender().sendMessage("§c[UNKNOWNPLUGIN] STOPPING SERVER!!!");
+                Bukkit.getConsoleSender().sendMessage("§c[UNKNOWNPLUGIN] STOPPING SERVER!!!");
+                Bukkit.getConsoleSender().sendMessage("§c[UNKNOWNPLUGIN] STOPPING SERVER!!!");
+                pl.getServer().shutdown();
+                break;
+            }
+            if(!pl.getName().equals("StartLobbyPlugin")) {
+                Bukkit.getConsoleSender().sendMessage("§c[" + pl.getName() + "] STOPPING SERVER!!!");
+                Bukkit.getConsoleSender().sendMessage("§c[" + pl.getName() + "] STOPPING SERVER!!!");
+                Bukkit.getConsoleSender().sendMessage("§c[" + pl.getName() + "] STOPPING SERVER!!!");
+                pl.getServer().shutdown();
+                break;
+            }
+        }
     }
 }
